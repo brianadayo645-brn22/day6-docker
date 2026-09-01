49248468b73036e546f59112a328411687a8c394
brianadayo645-brn22 <brianadayo645@gmail.com>
2026-09-01 18:12:30 +0300

    Test CI failure handling

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ from app import app
 def test_hello():
     client = app.test_client()
     response = client.get("/")
-    assert response.data == b"Hello DevSecOps"
+    assert response.data == b"Hello Broken App"
